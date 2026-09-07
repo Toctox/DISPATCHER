c9d930d2408b2faa549971900e1ff8d07cf9c645
Toctox <102564725+Toctox@users.noreply.github.com>
2026-09-06 22:53:35 -0300

    feat: export foreground ChatGPT desktop launcher

--- factory_dispatcher/launchers/__init__.py
+++ factory_dispatcher/launchers/__init__.py
@@ -1,6 +1,13 @@
 from .base import LaunchResult
 from .chatgpt_browser import BrowserChatGPTLauncher
+from .chatgpt_foreground import ForegroundChatGPTLauncher
 from .codex import CodexLauncher
 from .manual_chatgpt import ManualChatGPTLauncher
 
-__all__ = ["BrowserChatGPTLauncher", "CodexLauncher", "LaunchResult", "ManualChatGPTLauncher"]
+__all__ = [
+    "BrowserChatGPTLauncher",
+    "CodexLauncher",
+    "ForegroundChatGPTLauncher",
+    "LaunchResult",
+    "ManualChatGPTLauncher",
+]
