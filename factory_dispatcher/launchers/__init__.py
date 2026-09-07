28c4ca8c923713e9033f7a45471cd0ea63596c20
Toctox <102564725+Toctox@users.noreply.github.com>
2026-09-07 01:23:31 -0300

    fix: make guarded desktop launcher the default

--- factory_dispatcher/launchers/__init__.py
+++ factory_dispatcher/launchers/__init__.py
@@ -1,6 +1,6 @@
 from .base import LaunchResult
 from .chatgpt_browser import BrowserChatGPTLauncher
-from .chatgpt_foreground import ForegroundChatGPTLauncher
+from .chatgpt_foreground_v2 import GuardedForegroundChatGPTLauncher as ForegroundChatGPTLauncher
 from .codex import CodexLauncher
 from .manual_chatgpt import ManualChatGPTLauncher
 
