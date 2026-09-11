2d1257e03dd8c2d9ca89d1335f052df24c88f1e1
Toctox <102564725+Toctox@users.noreply.github.com>
2026-09-10 21:03:26 -0300

    feat(factory-bridge): route runtime commands through managed process tree

--- factory_bridge/main.go
+++ factory_bridge/main.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"runtime"
@@ -65,22 +64,7 @@ type runner interface {
 type osRunner struct{}
 
 func (osRunner) Run(ctx context.Context, spec runSpec) (string, string, int, error) {
-	cmd := exec.CommandContext(ctx, spec.exe, spec.args...)
-	cmd.Dir = spec.dir
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	code := 0
-	if err != nil {
-		var exitErr *exec.ExitError
-		if errors.As(err, &exitErr) {
-			code = exitErr.ExitCode()
-		} else {
-			code = -1
-		}
-	}
-	return truncate(stdout.String(), 128*1024), truncate(stderr.String(), 128*1024), code, err
+	return runManagedProcess(ctx, spec)
 }
 
 func truncate(s string, max int) string {
